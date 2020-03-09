6961e3b257a8b85fb4fb3478d382dbbe345ea325
HADJUR Hugo <hhadjur@em-lyon.com>
2020-03-09 14:33:35 +0100

    line separator param

--- main_beehives.py
+++ main_beehives.py
@@ -13,29 +13,31 @@ start_time = datetime.now()
 hostname = socket.gethostname()
 beehive_id = int(hostname[-1])
 
+line_separator = '\n--------------'
+
 serial_data = get_data_from_serial(init_serial())
-print(f'serial data,{datetime.now() - start_time}\n--------------')
+print(f'serial data,{datetime.now() - start_time}{line_separator}')
 prev_time = datetime.now()
 
 nb_img = 20
 path_to_img = capture_images(nb_img)
-print(f'capture {nb_img} images,{datetime.now() - prev_time}\n--------------')
+print(f'capture {nb_img} images,{datetime.now() - prev_time}{line_separator}')
 prev_time = datetime.now()
 
 resize_img(path_to_img)
-print(f'resize images,{datetime.now() - prev_time}\n--------------')
+print(f'resize images,{datetime.now() - prev_time}{line_separator}')
 prev_time = datetime.now()
 
 path_to_gif_file = convert_jpg_to_gif(path_to_img)
-print(f'convert jpg to gif,{datetime.now() - prev_time}\n--------------')
+print(f'convert jpg to gif,{datetime.now() - prev_time}{line_separator}')
 prev_time = datetime.now()
 
 gif_url = upload_to_imgur(beehive_id, path_to_gif_file)
-print(f'upload gif to imgur,{datetime.now() - prev_time}\n--------------')
+print(f'upload gif to imgur,{datetime.now() - prev_time}{line_separator}')
 prev_time = datetime.now()
 
 upload_to_spreadsheet(beehive_id, serial_data, gif_url)
-print(f'upload data to spreadsheet,{datetime.now() - prev_time}\n--------------')
+print(f'upload data to spreadsheet,{datetime.now() - prev_time}{line_separator}')
 prev_time = datetime.now()
 
 git_pull()
