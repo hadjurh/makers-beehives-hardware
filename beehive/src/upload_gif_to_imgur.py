00284a3ecee3948d7a3e92d240d936641868a777
HugoPi <hhadjur@em-lyon.com>
2020-03-11 09:02:42 +0100

    logs

--- beehive/src/upload_gif_to_imgur.py
+++ beehive/src/upload_gif_to_imgur.py
@@ -7,8 +7,10 @@ import datetime
 def upload_to_imgur(beehive_id, imgur_cred_path='imgur_credits.json', path_to_file='beehive/data/gif/capture.gif'):
     with open(imgur_cred_path) as imgur_credits_file:
         imgur_credits = json.load(imgur_credits_file)
+    print('Parsed imgur credentials')
 
     imgur = pyimgur.Imgur(imgur_credits['imgurClientID'])
+    print('Established connexion')
     image_title = 'MakersBeehive ' + str(beehive_id) + ' | ' + datetime.datetime.now().__str__()[:-7]
     uploaded_image = imgur.upload_image(path_to_file, title=image_title)
     image_link = uploaded_image.link
