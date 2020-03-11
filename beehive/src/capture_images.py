ccec40dba00e0d9b7de3307381dd1bc25fcdbb0e
HADJUR Hugo <hhadjur@em-lyon.com>
2020-03-11 13:40:05 +0100

    Thermal plot minor change + better resizing

--- beehive/src/capture_images.py
+++ beehive/src/capture_images.py
@@ -10,13 +10,15 @@ import os
 # Possible optimization: with resize parameter, "it is considerably more efficient
 # to have the Pi’s GPU perform the resizing when capturing the image".
 # https://picamera.readthedocs.io/en/release-1.13/recipes1.html#capturing-resized-images
-def capture_images(nb_img=20, path_to_img='beehive/data/pi_img', resize=None):
+def capture_images(nb_img=20, path_to_img='beehive/data/pi_img', resolution=None, resize=None):
     # Remove existing images
     for f in glob.glob(f'{path_to_img}/*.jpg'):
         os.remove(f)
 
     # Init
     camera = picamera.PiCamera()
+    if resolution is not None:
+        camera.resolution = resolution
 
     # Capture
     for i in range(nb_img):
