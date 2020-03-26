4529d47dfbd003e63de0bb02333766dd1358b050
Hugo <hugo.hadjur@gmail.com>
2020-03-26 14:45:34 +0100

    plot pixels vs time

--- energy_consumption/plot_energy_consumption.py
+++ energy_consumption/plot_energy_consumption.py
@@ -7,12 +7,19 @@ from glob import glob
 helvetica_bold = fm.FontProperties(fname='D:/Users/Hugo/Documents/asgbot/data/fonts/HelveticaNeueBold.ttf')
 helvetica = fm.FontProperties(fname='D:/Users/Hugo/Documents/asgbot/data/fonts/Helvetica-Normal.ttf')
 
-out_files = glob('energy_consumption/output/*.out')
+out_files = glob('energy_consumption/output/idle/*.out')
+
+# Plot
+fig = plt.figure(figsize=(21, 7))
+ax = fig.add_subplot(111)
+axes_labels_font = helvetica_bold
+axes_labels_font.set_size(25)
 
 for out_file in out_files:
+    print(out_file)
     # Only git pull
-    if 'compare_img_size_v2.out' not in out_file:
-        continue
+    # if 'compare_img_size_v2.out' not in out_file:
+    #     continue
 
     task_name = out_file.split('\\')[-1].split('.out')[0]
 
@@ -20,18 +27,11 @@ for out_file in out_files:
     df = pd.read_csv(out_file)
     df['timestamp'] = df['time'].apply(lambda t: datetime.datetime.strptime(t, "%Y-%m-%d %H:%M:%S.%f").timestamp())
     df['timestamp'] = df['timestamp'] - min(df['timestamp'])
-    print(max(df['timestamp']))
+    print(df['current'].mean())
 
-    # Plot
-    fig = plt.figure(figsize=(21, 7))
-    ax = fig.add_subplot(111)
-    axes_labels_font = helvetica_bold
-    axes_labels_font.set_size(25)
+    ax.step(df['timestamp'], df['current'], linewidth=2, where='post', zorder=6)
 
-    ax.step(df['timestamp'], df['current'], color='#007aff', linewidth=2, where='post', zorder=6)
-
-    # plt.ylim(320, 480)
-    # plt.xlim(xmax=32)
+    plt.ylim(230, 390)
     plt.xlabel('Time', fontproperties=helvetica_bold)
     plt.ylabel('Intensity (mA)', fontproperties=helvetica_bold)
     plt.title(f'Intensity evolution for: {task_name}', fontproperties=helvetica_bold, pad=20)
@@ -39,5 +39,5 @@ for out_file in out_files:
         label.set_fontproperties(axes_labels_font)
 
     fig.savefig(f'energy_consumption/plot/{task_name}_consumption.png', dpi=200)
-    plt.close(fig)
+plt.close(fig)
 
